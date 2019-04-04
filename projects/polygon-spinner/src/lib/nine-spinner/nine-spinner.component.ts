b434bdf2cd89f2337af425415ec98020a88343ef
Justin Haupt <jhaupt100@gmail.com>
2019-04-04 14:32:02 -0700

    add preset 9 spinner

--- projects/polygon-spinner/src/lib/nine-spinner/nine-spinner.component.ts
+++ projects/polygon-spinner/src/lib/nine-spinner/nine-spinner.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-nine-spinner',
   templateUrl: './nine-spinner.component.html',
   styleUrls: ['./nine-spinner.component.css']
 })
-export class NineSpinnerComponent implements OnInit {
+export class NineSpinnerComponent {
 
   readonly points = '50,24 66.6,30 75.4,45.3 72.4,62.7 58.8,74 41.2,74 27.6,62.7 24.6,45.3 33.4,30';
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
