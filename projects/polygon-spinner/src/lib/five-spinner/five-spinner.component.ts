9c5cfcc2f00e7862b25a9f1a8c79310f5c4eb501
Justin Haupt <jhaupt100@gmail.com>
2019-04-04 09:51:32 -0700

    add preset 5 polygon

--- projects/polygon-spinner/src/lib/five-spinner/five-spinner.component.ts
+++ projects/polygon-spinner/src/lib/five-spinner/five-spinner.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-five-spinner',
   templateUrl: './five-spinner.component.html',
   styleUrls: ['./five-spinner.component.css']
 })
-export class FiveSpinnerComponent implements OnInit {
+export class FiveSpinnerComponent {
+
+  readonly points = '50,25.2 73.9,42.6 64.8,70.8 35.2,70.8 26.1,42.6';
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
