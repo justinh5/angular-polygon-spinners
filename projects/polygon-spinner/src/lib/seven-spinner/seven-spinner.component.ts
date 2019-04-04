2067aa4c0410de0d952e5c76239b0e73414f3955
Justin Haupt <jhaupt100@gmail.com>
2019-04-04 11:09:26 -0700

    add preset 7 spinner

--- projects/polygon-spinner/src/lib/seven-spinner/seven-spinner.component.ts
+++ projects/polygon-spinner/src/lib/seven-spinner/seven-spinner.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-seven-spinner',
   templateUrl: './seven-spinner.component.html',
   styleUrls: ['./seven-spinner.component.css']
 })
-export class SevenSpinnerComponent implements OnInit {
+export class SevenSpinnerComponent {
+
+  readonly points = '50,25.2 69.7,34.7 74.6,56 61,73.2 39,73.2 25.4,56 30.3,34.7';
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
