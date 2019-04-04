f5d79651a873b7a73d9a3b0dd1851d23b7623cbf
Justin Haupt <jhaupt100@gmail.com>
2019-04-04 15:22:33 -0700

    add 10 polygon preset

--- projects/polygon-spinner/src/lib/ten-spinner/ten-spinner.component.ts
+++ projects/polygon-spinner/src/lib/ten-spinner/ten-spinner.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-ten-spinner',
   templateUrl: './ten-spinner.component.html',
   styleUrls: ['./ten-spinner.component.css']
 })
-export class TenSpinnerComponent implements OnInit {
+export class TenSpinnerComponent {
+
+  readonly points = '50,23.6 65.5,28.7 75.1,41.9 75.1,58.1 65.5,71.3 50,76.4 34.5,71.3 24.9,58.1 24.9,41.9 34.5,28.7';
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
